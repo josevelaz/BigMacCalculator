29b96925e309880f46706496ab86430f064b7258
Jose <dreamzzediting@gmail.com>
2020-09-30 10:42:01 -0400

    changed reqIp package

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express"
 import path from "path"
 import routes from "./routes"
 import cors from "cors"
+import requestIp from "request-ip"
 
 const app = express()
 const PORT = 8000
@@ -11,6 +12,7 @@ app.use(cors())
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"))
 }
+app.use(requestIp.mw())
 app.set("trust proxy", true)
 app.use(routes)
 
